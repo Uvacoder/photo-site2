de137b1bfe48eecbf5e3127c088f88cd73ac86db
kaylinm <kaylin.moss1@marist.edu>
2020-12-26 23:25:30 -0500

    added styling for active links and hamburger menu for mobile nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,8 @@
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import {
   StyledCenterSpan,
+  StyledHamburger,
   StyledLeftSpan,
   StyledNav,
   StyledNavLink,
@@ -9,34 +11,66 @@ import {
 } from "./NavigationStyles";
 import logo from "../../logo.png";
 
-const Navigation = () => {
-  return (
-    <StyledNav id="navContainer">
-      <StyledLeftSpan>
-        <Link to="/">
-          <StyledNavLogo src={logo} alt="logo" />
-        </Link>
-      </StyledLeftSpan>
-      <StyledCenterSpan>Capturing the emotions of the Earth</StyledCenterSpan>
-      <StyledRightSpan>
-        <StyledNavLink className="item" to="/nostalgia">
-          Nostalgia
-        </StyledNavLink>
-        <StyledNavLink className="item" to="/joy">
-          Joy
-        </StyledNavLink>
-        <StyledNavLink className="item" to="/despair">
-          Despair
-        </StyledNavLink>
-        <StyledNavLink className="item" to="/tranquility">
-          Tranquility
-        </StyledNavLink>
-        <StyledNavLink className="item" to="/about">
-          About
-        </StyledNavLink>
-      </StyledRightSpan>
-    </StyledNav>
-  );
-};
+export default class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.collapse = this.collapse.bind(this);
+  }
 
-export default Navigation;
+  collapse() {
+    const hamburger = document.querySelector(".hamburger");
+    hamburger.classList.toggle("is-active");
+  }
+
+  render() {
+    return (
+      <StyledNav id="navContainer">
+        <StyledLeftSpan>
+          <Link to="/">
+            <StyledNavLogo src={logo} alt="logo" />
+          </Link>
+        </StyledLeftSpan>
+        <StyledHamburger
+          className="hamburger hamburger--collapse"
+          type="button"
+          onClick={this.collapse}
+        >
+          <span className="hamburger-box">
+            <span className="hamburger-inner" />
+          </span>
+        </StyledHamburger>
+        <StyledCenterSpan>Capturing the emotions of the Earth</StyledCenterSpan>
+
+        <StyledRightSpan>
+          <StyledNavLink
+            className="item"
+            activeClassName="active"
+            to="/nostalgia"
+          >
+            Nostalgia
+          </StyledNavLink>
+          <StyledNavLink className="item" activeClassName="active" to="/joy">
+            Joy
+          </StyledNavLink>
+          <StyledNavLink
+            className="item"
+            activeClassName="active"
+            to="/despair"
+          >
+            Despair
+          </StyledNavLink>
+          <StyledNavLink
+            className="item"
+            activeClassName="active"
+            to="/tranquility"
+          >
+            Tranquility
+          </StyledNavLink>
+          <StyledNavLink className="item" activeClassName="active" to="/about">
+            About
+          </StyledNavLink>
+        </StyledRightSpan>
+      </StyledNav>
+    );
+  }
+}
