ed1afdad65e4d57b9e78a7eaa42f49d60411b175
kaylinm <kaylin.moss1@marist.edu>
2021-01-16 12:53:34 -0500

    added text to home page images

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,9 @@
 import React from "react";
 
+import "./Footer.css";
 const Footer = () => {
   return (
-    <section className="Footer">
+    <section className="footer">
       <p>&copy;COPYRIGHT KAYLIN MOSS 2021</p>
     </section>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./Home.css";
 import home1 from "./home1.png";
@@ -11,19 +12,30 @@ const Home = () => {
     <main id="home">
       <section className="home">
         <div className="column">
-          <img src={home1} alt="sunrise bokeh" />
-          <img
-            src={home2}
-            alt="Green mountains and a cloudy blue sky on a bright summer day"
-          />
-          <img
-            src={home3}
-            alt="Two young women sitting on the edge of a mountain with their backs turned on an overcast, cloudy day"
-          />
-          <img
-            src={home4}
-            alt="A sun flare reflects on a blue body of water under a pier"
-          />
+          <Link to="/nostalgia">
+            <img src={home1} alt="sunrise bokeh" />
+          </Link>
+
+          <Link to="/joy">
+            <img
+              src={home2}
+              alt="Green mountains and a cloudy blue sky on a bright summer day"
+            />
+          </Link>
+
+          <Link to="/despair">
+            <img
+              src={home3}
+              alt="Two young women sitting on the edge of a mountain with their backs turned on an overcast, cloudy day"
+            />
+          </Link>
+
+          <Link to="/tranquility">
+            <img
+              src={home4}
+              alt="A sun flare reflects on a blue body of water under a pier"
+            />
+          </Link>
         </div>
       </section>
     </main>
